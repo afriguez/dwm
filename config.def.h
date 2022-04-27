69bb2db5e957be30b23144b28e3c1fbb67a52618
Fer <afriguez@gmail.com>
2022-04-27 17:30:26 -0500

    feat: Change Font & gappx

--- config.def.h
+++ config.def.h
@@ -2,15 +2,15 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const unsigned int colorfultag    = 1;  /* 0 means use SchemeSel for selected tag */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "JetBrains Mono Medium:size=10" };
-static const char dmenufont[]       = "JetBrains Mono Medium:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:Medium:size=15" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:Italic:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
