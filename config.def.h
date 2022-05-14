ac1b3b8a5faf5680abe61413e239610016aeb74b
Fer <afriguez@gmail.com>
2022-05-14 12:53:28 -0500

    feat: Change bar padding & border pixel of windows

--- config.def.h
+++ config.def.h
@@ -1,14 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const unsigned int colorfultag    = 1;  /* 0 means use SchemeSel for selected tag */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int vertpad            = 5;       /* vertical padding of bar */
+static const int sidepad            = 5;       /* horizontal padding of bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:Medium:size=15" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:Italic:size=15";
 static const char col_gray1[]       = "#222222";
